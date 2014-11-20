7d6749942b330e1b31806785dd84fb575bb6025f
nbt11nsn <niklassjogren_@hotmail.com>
2014-11-20 14:07:35 +0100

    commit1

---
+++ admin/header.php
@@ -0,0 +1,1 @@
+<img src= "../image/Mobow.PNG" id = "mobowpic">
---
+++ admin/menuebar.php
@@ -0,0 +1,13 @@
+
+<div id = "choice">
+<nav>
+    <ul class="fancyNav">
+		<li><a href = "contract.php"> Kontrakt</a></li> 
+		<li><a href = "invoice.php"> Faktura</a></li>
+		<li><a href = "report.php"> Felrapportering</a></li>
+		<li><a href = "edit.php"> Editera inlogg</a></li>
+		<li><a href = "order.php"> Beställning</a></li>
+		<li><a href = "companymessage.php"> Från företag</a></li>
+	</ul> 
+</nav>
+</div>
