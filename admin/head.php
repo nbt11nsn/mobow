4285b8b51005a02fde00553c2be4d8b1370a5467
Rickard Hedlund <bigricke@hotmail.com>
2014-11-21 11:38:04 +0100

    update

--- admin/head.php
+++
@@ -1,7 +0,0 @@
-<meta charset=utf-8 />
-<title></title>
-<link rel="stylesheet" type="text/css" media="screen" href="css/index.css" />
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-<!--[if IE]>
-<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
--- admin/header.php
+++
@@ -1,1 +0,0 @@
-<img src= "../image/Mobow.PNG" id = "mobowpic">
