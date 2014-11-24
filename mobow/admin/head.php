46172ca3c1343925e53f59658c84b89033bf554a
Rickard Hedlund <bigricke@hotmail.com>
2014-11-24 15:24:28 +0100

    CSS

--- mobow/admin/head.php
+++ mobow/admin/head.php
@@ -1,5 +1,6 @@
 <meta charset=utf-8 />
 <title></title>
+<link rel="stylesheet" type="text/css" media="screen" href="css/default.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="css/index.css" />
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 <!--[if IE]>
