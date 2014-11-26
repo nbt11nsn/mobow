65d498c23a2b658774c1800b1e1463b9fb84d697
Rickard Hedlund <bigricke@hotmail.com>
2014-11-26 11:35:42 +0100

    sql updatering

--- mobowdb.sql
+++ mobowdb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 26, 2014 at 10:40 AM
+-- Generation Time: Nov 26, 2014 at 11:35 AM
 -- Server version: 5.5.40-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.5
 
@@ -19,6 +19,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `mobowdb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adress`
+--
+
+CREATE TABLE IF NOT EXISTS `adress` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `postnr` int(11) DEFAULT NULL,
+  `stad` varchar(100) NOT NULL,
+  `gata` varchar(100) NOT NULL,
+  `long` double NOT NULL,
+  `lat` double NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `adress`
+--
+
+INSERT INTO `adress` (`ID`, `postnr`, `stad`, `gata`, `long`, `lat`) VALUES
+(1, 80320, 'Gävle', 'Stortorget 1', 17.1420337, 60.6748389),
+(2, 91232, 'Vilhelmina', 'Postgatan 3', 16.6547774, 64.6230765),
+(3, 77670, 'Vikmanshyttan', 'Rostugnsvägen 3', 15.8253629, 60.2930402),
+(4, 95391, 'Haparanda', 'Kukkolaforsen 184', 24.0548527, 65.9591007),
+(5, 95336, 'Haparanda', 'Norrskensvägen 2', 24.1324517, 65.8429222);
 
 -- --------------------------------------------------------
 
