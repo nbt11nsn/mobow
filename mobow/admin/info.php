e0b5d27fa167def95a8dd7a3c39d78f69bb4bdd3
nbt11nsn <niklassjogren_@hotmail.com>
2014-12-02 16:14:10 +0100

    påbörjad

--- mobow/admin/info.php
+++ mobow/admin/info.php
@@ -4,6 +4,9 @@ SESSION_start();
 <!DOCTYPE html>
 <html>
 <head>
+<script src= "js/blink.js"></script>
+<link rel="stylesheet" type="text/css" media="screen" href="css/default.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="css/index.css" />
 <?php
 // tar bort all form av felrapportering
 // defined('THE_ERROR') || define('THE_ERROR', TRUE);
@@ -31,10 +34,34 @@ require_once("include/menuebar.php");
 ?>
 
 </div><!--main-wrapper-->
+<div id="frame">
+ <div id = "overviewinfo">
+hej
+</div>
+   	<form>
+		<div>
+		<input type="button" name="btn2" id="btn" style="width: 300px; height: 50px;" value="En felrapport har skapats" onclick="location.href='report.php'"/>
+		<input type="button" name="btn2" id="btn" style="width: 300px; height: 50px;" value="En kund vill ändra sin information" onclick="location.href='companymessage.php'"/>
+		</form>
+		</div>
+    </form>
+  </div>
+</div>
+
 <?php
 defined('THE_FOOTER') || define('THE_FOOTER', TRUE);
 require_once("include/footer.php");
 ?>
+
+<?php // Anteckningar
+
+// knapparna skall komma upp endast om ett företag har gjort en felrapport/editerat info. 
+// **KLAR**dirigera vidare vid ett knapptryck till rätt flik
+?>
+
+
 </body>
 </html>
+
+
 
