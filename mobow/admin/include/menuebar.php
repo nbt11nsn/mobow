f4904e0ce9789ece23ce13c7c3c320446b8ddd3f
Rickard Hedlund <bigricke@hotmail.com>
2014-12-04 17:57:49 +0100

    fixat menyn så den har undermenyer

--- mobow/admin/include/menuebar.php
+++ mobow/admin/include/menuebar.php
@@ -3,31 +3,20 @@
 ?>
 <div id = "huvudmeny">
   <nav>
-    <ul class="fancyNav">
-      <li>
-        <a href="info.php" class="hemikon">H</a>
-      </li> 
-      <li>
-      <a href="contract.php">Kontrakt</a>
-      </li> 
-      <li>
-      <a href="invoice.php">Faktura</a>
+    <ul>
+      <li><a href="info.php" class="hemikon">H</a></li> 
+      <li><a href="contract.php">Kontrakt</a></li> 
+      <li><a href="invoice.php">Faktura</a></li>
+      <li><a href="report.php">Felrapportering</a></li>
+      <li class="gotsub"><a href="#">Editera</a>
+        <ul>
+          <li><a href="edit.php">Inloggning</a></li>
+          <li><a href="#">Öppettider</a></li>
+        </ul>
       </li>
-      <li>
-      <a href="report.php">Felrapportering</a>
-      </li>
-      <li>
-      <a href="edit.php">Editera inlogg</a>
-      </li>
-      <li>
-      <a href="order.php">Beställning</a>
-      </li>
-      <li>
-      <a href="companymessage.php">Från företag</a>
-      </li>
-      <li>
-      <a href="logout.php" class="logikon">L</a>
-      </li>
+      <li><a href="order.php">Beställning</a></li>
+      <li><a href="companymessage.php">Meddelande</a></li>
+      <li><a href="logout.php" class="logikon">L</a></li>
     </ul>
   </nav>
 </div>
