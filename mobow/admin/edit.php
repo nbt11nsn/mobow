6bb9ddab2c90a73fd224bcb0ab483656f3bb3222
Rickard Hedlund <bigricke@hotmail.com>
2014-12-05 14:08:36 +0100

    små fix

--- mobow/admin/edit.php
+++ mobow/admin/edit.php
@@ -21,27 +21,25 @@ require_once("include/header.php");
 defined('THE_MENUE') || define('THE_MENUE', TRUE);
 require_once("include/menuebar.php");
 ?>
-<div id = "frame">
-  <form action="" method="post" id = "passchange">
-    <ul>
-      <li>
-	<label for="gpasswd">Gammalt lösenord: </label>
-	<input type="password" id="gpasswd" maxlength="50" required autofocus />
-      </li>
-      <li>
-	<label for="npasswd">Nytt lösenord: </label>
-	<input type="password" id="npasswd" maxlength="50" required />
-      </li>
-      <li>
-	<label for="cpasswd">Upprepa nytt lösenord: </label>
-	<input type="password" id="cpasswd" maxlength="50" required />
-      </li>
-      <li class="submit">
-	<input type="submit" value="Uppdatera" />
-      </li>
-    </ul>
-  </form>
-</div><!--frame-->
+<div id = "frame_edit">
+    <form action="" method="post" id = "passchange">
+	<ul>
+	    <li>
+		<label for="gpasswd">Gammalt lösenord: </label>
+		<input type="password" id="gpasswd" maxlength="50" required autofocus />
+	    </li>
+	    <li>
+		<label for="npasswd">Nytt lösenord: </label>
+		<input type="password" id="npasswd" maxlength="50" required />
+	    </li>
+	    <li>
+		<label for="cpasswd">Upprepa nytt lösenord: </label>
+		<input type="password" id="cpasswd" maxlength="50" required />
+	    </li>
+	    <li class="submit"><input type="submit" value="Uppdatera" /></li>
+	</ul>
+    </form>
+</div><!--frame_edit-->
 </div><!--main-wrapper-->
 <?php
 defined('THE_FOOTER') || define('THE_FOOTER', TRUE);
