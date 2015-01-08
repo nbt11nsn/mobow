f755950c2ecf47f804e5573bc426e670f72d4514
kalle605 <kalle605@live.se>
2015-01-08 14:06:01 +0100

    ny meny

--- public_html/admin/newcontact.php
+++ public_html/admin/newcontact.php
@@ -1,4 +1,37 @@
+<?php
+SESSION_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+defined('THE_SESSION') || define('THE_SESSION', TRUE);
+require_once('include/checksession.php');
+defined('THE_ASESSION') || define('THE_ASESSION', TRUE);
+require_once('include/checkasession.php');
+defined('THE_HEAD') || define('THE_HEAD', TRUE);
+include_once("include/head.php");
+?>
 
+</head>
+<body>
+<?php
+defined('THE_HEADER') || define('THE_HEADER', TRUE);
+require_once("include/header.php");
+?>
+<div id="main-wrapper">
+<?php
+defined('THE_MENUE') || define('THE_MENUE', TRUE);
+require_once("include/menuebar.php");
+defined('THE_DB') || define('THE_DB', TRUE);
+require_once(__DIR__ .'./../../db.php');
+$getCompanies = "SELECT * FROM foretag";
+$getContacts = "SELECT * FROM kontaktperson";
+$getIcons = "SELECT * FROM ikontyp";
+?>
+
+<div id = "frame">
+    <form action='' method='post' id ='postContracts' enctype="multipart/form-data">
 <fieldset>
 <legend><b>Användare</b></legend>
 <li>
@@ -26,3 +59,6 @@
 <input required type="text"  align="left" value = "" maxlength="100" name="password" id="password" />
 </li>
 </fieldset>
+<input type="reset" name="rst" id="rst" value="Återställ" />
+<input type="submit" name="save" id="save" value="Spara" />
+</frame>
