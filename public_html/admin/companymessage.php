e8a33e45369b1042ddc10d104f2205723873a58a
Rickard Hedlund <bigricke@hotmail.com>
2015-01-08 15:30:40 +0100

    admin meddelande

--- public_html/admin/companymessage.php
+++ public_html/admin/companymessage.php
@@ -25,14 +25,67 @@ require_once("include/menuebar.php");
 <?php 
 if(isset($_SESSION['admin']) && $_SESSION['admin'])
 {
+    $useracc = "Användarkonto";
+    $contract = "Kontrakt";
+    $type = "Typ:";
+    $req = "Begärd förändring på:";
+    $status = "Status:";
+    $smsg = "meddelande";
+    $sumsg = "oläst meddelande";
+    $pmsg = "meddelanden";
+    $pumsg = "olästa meddelanden";
+
     $sqlcount = "SELECT (SELECT COUNT(*) FROM edit_foretag) + (SELECT COUNT(*) FROM edit_kntper) AS numberofmessage FROM DUAL";
     $rescount = mysqli_query($con, $sqlcount);
     $asscount = mysqli_fetch_assoc($rescount);
     $nrofmessage = $asscount['numberofmessage'];
-    $sqlknt = "SELECT * FROM edit_kntper";
-    $resknt = mysqli_query($con, $sqlknt);
-    $sqlforetag = "SELECT * FROM edit_foretag";
-    $resforetag = mysqli_query($con, $sqlforetag);
+
+    $sqluncount = "SELECT (SELECT COUNT(*) FROM edit_foretag WHERE status = '1') + (SELECT COUNT(*) FROM edit_kntper WHERE status = '1') AS numbermsg FROM DUAL";
+    $resuncount = mysqli_query($con, $sqluncount);
+    $assuncount = mysqli_fetch_assoc($resuncount);
+    $nrofunreadmsg = $assuncount['numbermsg'];
+
+    if($nrofmessage < 1) // Det finns inga meddelanden
+    {
+        echo"Det finns inga meddelanden att visa";
+    }
+    else //Det finns meddelanden
+    {
+        echo"<table>";
+        echo"<tr><td></td>";
+        if($nrofunreadmsg < 2){echo"<td>$nrofunreadmsg $sumsg.</td>";}
+        else{echo"<td>$nrofunreadmsg $psmsg.</td>";}
+        if($nrofmessage < 2){echo"<td>$nrofmessage $smsg.</td>";}
+        else{echo"<td>$nrofmessage $pmsg.</td>";}
+        echo"</tr>";
+        echo"<tr><th>$type</th><th>$req</th><th>$status</th></tr>";
+        $sqlknt = "SELECT edit_kntper.kontaktid AS kid, medstatus.info AS info FROM edit_kntper LEFT OUTER JOIN medstatus ON medstatus.ID = edit_kntper.status";
+        $sqlforetag = "SELECT edit_foretag.kontraktid AS kid, kontrakt.kontorsnamn AS namn, medstatus.info AS info FROM edit_foretag LEFT OUTER JOIN kontrakt ON edit_foretag.kontraktid = kontrakt.ID LEFT OUTER JOIN medstatus ON medstatus.ID = edit_foretag.status";
+        $resknt = mysqli_query($con, $sqlknt);
+        $resforetag = mysqli_query($con, $sqlforetag);
+        while($row = mysqli_fetch_assoc($resknt))
+        {
+            $id = $row['kid'];
+            $info = $row['info'];
+            echo"<tr>
+            <td>$useracc</td>
+            <td>$id</td>
+            <td>$info</td>
+            </tr>";
+        }
+        while($row = mysqli_fetch_assoc($resforetag))
+        {
+            $id = $row['kid'];
+            $namn = $row['namn'];
+            $info = $row['info'];
+            echo"<tr>
+            <td>$contract</td>
+            <td>$namn</td>
+            <td>$info</td>
+            </tr>";
+        }
+        echo"</table>";
+    }
 }
 else
 {
